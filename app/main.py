e7c8608f8c0355f81f6301e16302c0da99014e87
Prideland-Okoi <prideland.okoi@gmail.com>
2024-08-05 12:16:25 +0100

    Update main.py
    checks for only url. remove checks for html

--- app/main.py
+++ app/main.py
@@ -1,14 +1,34 @@
-from fastapi import FastAPI, HTTPException
-from .models import URLRequest
+from fastapi import FastAPI, Form, HTTPException
+from typing import Optional
+from urllib.parse import urlparse, urlunparse
 from .utils import fetch_html_content, check_labels
+from .models import URLRequest
+from collections import Counter
 
 app = FastAPI()
 
+def ensure_url_scheme(url: str) -> str:
+    """Ensure http:// is prefixed to the string"""
+    parsed_url = urlparse(url)
+    if not parsed_url.scheme:
+        return urlunparse(parsed_url._replace(scheme='http'))
+    return url
+
 @app.post("/check")
-async def check(url_request: URLRequest):
-    html_content = fetch_html_content(url_request.url)
-    if html_content is None:
-        raise HTTPException(status_code=400, detail="Failed to fetch HTML content from the provided URL.")
+async def check(url: Optional[str] = Form(None)):
+    if url:
+        url = ensure_url_scheme(url)
     
-    report = check_labels(html_content)
-    return report
+    url_request = URLRequest(url=url)
+
+    if url_request.url:
+        html_content = fetch_html_content(url_request.url)
+        if html_content is None:
+            raise HTTPException(status_code=400, detail="Failed to fetch HTML content from the provided URL.")
+    else:
+        raise HTTPException(status_code=400, detail="A URL must be provided.")
+
+    # report = check_labels(html_content)
+    # return {"report": report}
+    report, error_summary = check_labels(html_content)
+    return {"report": report, "error_summary": error_summary}
