155949ee06d530e547cabe6b26494287039775c8
Prideland-Okoi <prideland.okoi@gmail.com>
2024-08-05 12:17:19 +0100

    Update models.py
    Add a validator for url

--- app/models.py
+++ app/models.py
@@ -1,4 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl, validator
+from typing import Optional
 
 class URLRequest(BaseModel):
-    url: str
+    url: Optional[HttpUrl] = None
+
+    @validator("url", pre=True, always=True)
+    def validate_and_correct_url(cls, value):
+        if value and not str(value).startswith(('http://', 'https://')):
+            value = 'http://' + str(value)
+        return value
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "url": "http://example.com"
+            }
+        }
